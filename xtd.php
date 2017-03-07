ce53da19a89e0f724f036e922028e82f1d949f8f
Petr Valenta <justreverse@gmail.com>
2017-03-07 14:49:54 +0100

    options handling

--- xtd.php
+++ xtd.php
@@ -1,29 +1,96 @@
 #!/usr/bin/php
 <?php
 
-loadSettings($argv);
+get_opts($argv);
 
-function loadSettings($argv){
-    $options = "abg";
+function get_opts($argv){
+    $options = "a";  // negenerovat sloupce z atributu     - $columns
+    $options .= "b"; // sloucit podelementy stejneho nazvu - $group
+    $options .= "g"; // vygenerovat xml s relacemi         - $relations
+
     $longopts = array(
-      "help",
-      "input::",
-      "output::",
-      "header::",
-      "etc::",
+      "help",        // vypsat help
+      "input:",      // vstupni soubor                     - $input_file
+      "output:",     // vystupni soubor                    - $output_file
+      "header:",     // text hlavicky                      - $header_text
+      "etc:",        // maximalni pocet sloupcu            - $columns_max
     );
 
-    unset($argv[0]); //zbavime se prvniho argumentu - jmena skriptu
     $opts = getopt($options, $longopts);
 
-    $all = getopt(implode('', array_merge(range('a', 'z'), range('A', 'Z'), range('0', '9'))));
-    $wrongOptions = array_diff(array_keys($all), array_keys($opts));
-    if (!empty($wrongOptions)) {
-      echo("Wrong options: " . implode(', ', $wrongOptions));
-      exit(1);
+
+    global $columns = 0, $group = 0, $relations = 0, $input_stream, $output_stream, $header_text, $columns_max = -1;
+
+    foreach($opts as $key => $value){
+      switch ($key){
+          case "a":
+              if($value === FALSE) $columns = 1;
+              break;
+
+          case "b":
+              if($value === FALSE) $group = 1;
+              break;
+
+          case "g":
+              if($value === FALSE) $relations = 1;
+              break;
+
+          case "help":
+              if($value === FALSE) print_help();
+              break;
+
+          case "input":
+              $input_file = $value
+              break;
+
+          case "output":
+              $output_file = $value;
+              break;
+
+          case "header":
+              $header_text = $value;
+              break;
+
+          case "etc":
+              $columns_max = intval($value);
+              if(!is_int($columns_max)) print_error("--etc value is not int" , 1);
+              break;
+      }
     }
 
+    // overime konflikt argumentu
+    if($columns_max && $group) print_error("--etc cannot be set with -b", 1);
+
+    // overime vstupni a vystupni soubory
+    if($input_file){
+      $input_stream = fopen($input_file);
+    } else {
+      $input_stream = STDIN;
+    }
+
+    if($output_file){
+      fopen(
+    } else {
+      $output_stream = STDOUT;
+    }
+
+
+
+
+
     var_dump($opts);
+}
+
+
+
+function print_help(){
+  echo("HELP,\nTODO TODO TODO\nTODO TODO TODO \nTODO TODO TODO\n")
+  exit(0);
+}
+
+function print_error($text, $code){
+  echo("ERROR " . $code . " : " . $text)
+  exot($code);
 }
 
 
