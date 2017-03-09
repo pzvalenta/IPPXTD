4f05d1664d576844aed0cab56fcded7b8f734204
Petr Valenta <justreverse@gmail.com>
2017-03-09 22:06:13 +0100

    added table class and functions

--- xtd.php
+++ xtd.php
@@ -25,23 +25,138 @@ fclose($output_stream);
 exit(0); //TODO
 
 function generate_unlimited(){
-  global $xml, $columns, $group, $relations, $output_stream, $header_text, $columns_max;
+  global $tables, $xml, $columns, $group, $relations, $output_stream, $header_text, $columns_max;
 
-
+  $tables = array();
   recursive_print_children($xml, 0);
 
+  print_tables($tables);
 }
 
 
 function recursive_print_children($parent, $depth){
+  global $tables;
+  $new_table = new table;
+
+  //////////////////////////// pomocny kod
   for($i = 0; $i < $depth; $i++){
     if($i === $depth - 1) echo("|___");
     else echo("\t");
   }
-  echo($parent->getName() . "\n");
+  echo($parent->getName() . "( ");
+  foreach ($parent->attributes() as $attribute) {
+    echo($attribute->getName()." ");
+  }
+  echo(")\n");
+  ///////////////////////////
+
+
+
+  foreach($parent->attributes() as $attribute){
+    $new_table->addAttribute($attribute->getName(), "PLACEHOLDER INT");
+  }
+
   foreach($parent->children() as $child){
+    $new_table->addSubelement($child->getName(), "PLACEHOLDER INT");
     recursive_print_children($child, $depth + 1);
   }
+
+  if (!isset($tables[$parent->getName()])){
+    $new_table->setName($parent->getName());
+    $tables[$parent->getName()] = $new_table;
+  } else {
+    // TODO
+    // porovnat tabulky, sloucit je
+    merge_tables($new_table, $tables[$parent->getName()]);
+  }
+
+}
+
+function print_tables($tables){
+  foreach ($tables as $table) {
+    $table->print_table();
+  }
+}
+
+
+
+class table {
+  public $name = "";
+  public $subelements = array();
+  public $attributes = array();
+  public $primary_key;
+
+  public function getName(){
+    return $this->name;
+  }
+
+  public function setName($text){
+    $this->name = $text;
+    $this->primary_key = "prk_" . $text . "_id INT PRIMARY KEY";
+  }
+
+  public function addAttribute($name, $type){
+    if (isset($this->attributes[$name . "_id"])) print_error("attribute name colision", 90);
+    // TODO je toto chyba?
+    $this->attributes[$name] = $type;
+  }
+
+  public function addSubelement($name, $type){
+    if (isset($this->attributes[$name . "_id"])) print_error("attribute and subelemnt name colision", 90);
+
+    if (isset($this->subelements[$name . "_id"])){
+      $this->subelements[$name . "1_id"] = $this->subelements[$name . "_id"];
+      unset($this->subelements[$name . "_id"]);
+      $this->subelements[$name . "2_id"] = $type;
+    } elseif (isset($this->subelements[$name . "1_id"])) {
+      $i = 2;
+      for($i; isset($this->subelements[$name . $i . "_id"]); $i++){}
+      $this->subelements[$name . $i . "_id"] = $type;
+    } else {
+      $this->subelements[$name . "_id"] = $type;
+    }
+
+  }
+
+  public function print_table(){
+    echo("CREATE TABLE " . $this->name . "(\n");
+    echo("\t$this->primary_key");
+    foreach ($this->attributes as $name => $type) {
+      echo(",\n");
+      echo("\t" . $name . " " . $type);
+    }
+
+    foreach ($this->subelements as $name => $type) {
+      echo(",\n");
+      echo("\t" . $name . " " . $type);
+    }
+
+    echo("\n");
+    echo(");\n\n");
+  }
+}
+
+// sloucit tabulku1 do tabulky2
+function merge_tables($table1, $table2){
+  foreach ($table1->attributes as $name1 => $type1) {
+    if (isset($table2->attributes[$name1])){
+      // TODO check type
+    } else {
+      $table2->attributes[$name1] = $type1;
+    }
+  }
+
+  foreach ($table1->subelements as $name1 => $type1) {
+    if (isset($table2->subelements[$name1])){
+      // TODO check type
+    } else {
+      $table2->subelements[$name1] = $type1;
+    }
+  }
+}
+
+function stronger_type($type1, $type2){
+  return "PLACEHOLDER TYPE";
 }
 
 
